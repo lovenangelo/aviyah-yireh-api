53f9a42b651190fdbf3b0705cdfcf936ce59cda5
lovenangelo <lovenangelo.dev>
2025-06-24 11:48:42 +0800

    feat: swagger setup

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,22 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="NuWa Stone API",
+ *      description="API Docs",
+ *
+ *      @OA\Contact(
+ *          email="lovenangelo.dev@gmail.com"
+ *      ),
+ *
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
