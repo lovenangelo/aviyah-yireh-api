d5be1efd3dcb3347ea6e6196f343eda0836ed636
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-07-22 11:35:32 +0800

    refactor: 2fa verification uses apiresponse trait

--- app/Http/Requests/Auth/TwoFactorVerifyRequest.php
+++ app/Http/Requests/Auth/TwoFactorVerifyRequest.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Requests\Auth;
 
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use App\Traits\ApiResponse;
 use Illuminate\Foundation\Http\FormRequest;
 
 class TwoFactorVerifyRequest extends FormRequest
 {
+  use ApiResponse;
+
   /**
    * Determine if the user is authorized to make this request.
    */
@@ -13,6 +18,7 @@ class TwoFactorVerifyRequest extends FormRequest
   {
     return true;
   }
+
 
   /**
    * Get the validation rules that apply to the request.
@@ -25,5 +31,11 @@ class TwoFactorVerifyRequest extends FormRequest
       'code' => 'required|string|size:6',
       'email' => 'required|string|email',
     ];
+  }
+
+  protected function failedValidation(Validator $validator)
+  {
+    $response = $this->formatErrorResponse(422, 'Validation failed.', $validator->errors()->toArray());
+    throw new HttpResponseException($response);
   }
 }
