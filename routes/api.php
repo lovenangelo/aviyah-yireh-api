607f11fc003eec19e862b9f434dbfda5053dd975
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-07-22 15:26:02 +0800

    feat: restful routes for roles

--- routes/api.php
+++ routes/api.php
@@ -74,11 +74,29 @@ Route::prefix('v1')->group(function () {
         // User API resource
         Route::apiResource('/users', UserAPIController::class);
 
-        // Role bulk delete
-        Route::delete('/roles/bulk-delete', [RoleAPIController::class, 'bulkDestroy'])
-            ->name('roles.bulk-delete');
+        // Roles-related Routes
+        Route::prefix("roles")->group(function () {
+            // Roles list
+            Route::get("/", [RoleAPIController::class, 'index'])->name('roles.list');
 
-        // Role API resource
-        Route::apiResource('/roles', RoleAPIController::class);
+            // Role retrieve
+            Route::get("/{id}", [RoleAPIController::class, 'show'])->name("role.retrieve");
+
+            // Role create
+            Route::post("/", [RoleAPIController::class, 'store'])->name('roles.create');
+
+            // Role update
+            Route::put("/{id}", [RoleAPIController::class, 'update'])->name("roles.update");
+
+            // Role delete
+            Route::delete("/{id}", [RoleAPIController::class, 'destroy'])->name("roles.delete");
+
+            // Role bulk delete
+            Route::delete('/bulk-delete', [RoleAPIController::class, 'bulkDestroy'])
+                ->name('roles.bulk-delete');
+
+            // Role API resource
+            Route::apiResource('/', RoleAPIController::class);
+        });
     });
 });
