cd6744fadc256fbb1e19174975995c80b438c3a8
lovenangelo <lovenangelo.dev>
2025-08-05 13:31:00 +0800

    refactor: response data for pagination

--- app/Http/Resources/CustomPaginatedCollection.php
+++ app/Http/Resources/CustomPaginatedCollection.php
@@ -17,14 +17,19 @@ class CustomPaginatedCollection extends ResourceCollection
     public function toArray($request)
     {
         $paginated = $this->resource->toArray();
-
         if (isset($paginated['data'])) {
-
             $paginated['items'] = $paginated['data'];
             unset($paginated['data']);
-
             if (!$this->includeLinks) {
                 unset($paginated['links']);
+                unset($paginated['last_page']);
+                unset($paginated['last_page_url']);
+                unset($paginated['first_page_url']);
+                unset($paginated['prev_page_url']);
+                unset($paginated['from']);
+                unset($paginated['to']);
+                unset($paginated['path']);
+                unset($paginated['per_page']);
             }
         }
 
