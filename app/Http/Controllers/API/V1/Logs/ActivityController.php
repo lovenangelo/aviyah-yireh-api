107891785a64050f9034839f4267023fd1d2901c
lovenangelo <lovenangelo.dev>
2025-08-09 15:26:10 +0800

    refactor: paginated log response

--- app/Http/Controllers/API/V1/Logs/ActivityController.php
+++ app/Http/Controllers/API/V1/Logs/ActivityController.php
@@ -2,29 +2,28 @@
 
 namespace App\Http\Controllers\API\V1\Logs;
 
-use Illuminate\Http\Request;
-use App\Models\Events;
 use App\Traits\ApiResponse;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\JsonResponse;
-use App\Http\Requests\Event\StoreEventRequest;
-use App\Http\Requests\Event\UpdateEventRequest;
-use App\Http\Requests\Event\BulkDeleteEventRequest;
 use App\Http\Resources\CustomPaginatedCollection;
-use App\Repositories\EventRepository;
-use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Spatie\Activitylog\Models\Activity;
+
 class ActivityController extends Controller
 {
+    use ApiResponse;
     public function getAllLogs(): JsonResponse
     {
         try {
-            $activities = Activity::all();
-            return $this->formatSuccessResponse($activities, "Activity logs retrieved successfully");
+            $perPage = request()->get('per_page', 15);
+            $activities = Activity::paginate($perPage);
+
+            // Assuming you have a CustomPaginatedCollection resource
+            return $this->formatSuccessResponse(
+                new CustomPaginatedCollection($activities),
+                "Activity logs retrieved successfully"
+            );
         } catch (\Throwable $e) {
             return $this->handleApiException($e, request(), "Fetching Activity Logs");
         }
     }
-
-
 }
